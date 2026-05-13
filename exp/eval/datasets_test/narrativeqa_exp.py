3ca0d119ba1b02d243fc5aa86eb3218f4ab9f65f
NeurIPS-2026-code <19966189260@163.com>
2026-05-13 10:43:28 +0800

    Update narrativeqa_exp.py

--- exp/eval/datasets_test/narrativeqa_exp.py
+++ exp/eval/datasets_test/narrativeqa_exp.py
@@ -10,12 +10,11 @@ import random
 from typing import Any, Counter, Dict, List, Optional, Tuple
 from tqdm import tqdm
 
+from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT_DIR))
 
 from exp.eval.research_agent_exp_main import ResearchAgent_exp
-
-
 
 from gam import (
     MemoryAgent,
