e0fe67c7fd8549587a0e00566f02954eb618f26c
NeurIPS-2026-code <19966189260@163.com>
2026-07-25 16:43:41 +0800

    Update self_reflection.py

--- exp/self_reflection.py
+++ exp/self_reflection.py
@@ -1,9 +1,6 @@
 import sys
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).resolve().parents[2]
-sys.path.append(str(ROOT_DIR))
-
 import json
 from openai import OpenAI
 from gam.generator import AbsGenerator
